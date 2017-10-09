f260cae6d3ac69b2ea5910a718bbb50edf3a2da4
Christian Huitema <huitema@huitema.net>
2017-10-08 21:40:03 -0700

    Adding the two connections test to the linux tester

--- picoquic_t/picoquic_t.c
+++ picoquic_t/picoquic_t.c
@@ -59,7 +59,8 @@ static picoquic_test_def_t test_table[] = {
     { "tls_api_very_long_congestion", tls_api_very_long_congestion_test },
     { "http0dot9", http0dot9_test },
     { "hrr", tls_api_hrr_test },
-    { "ack_of_ack", ack_of_ack_test }
+    { "ack_of_ack", ack_of_ack_test },
+    { "two_connections", tls_api_two_connections_test }
 };
 
 static size_t nb_tests = sizeof(test_table) / sizeof(picoquic_test_def_t);
