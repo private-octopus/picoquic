57ff7cd1adffa3d0ced2aee0bfc6b04173d57ef5
huitema <huitema@huitema.net>
2018-08-07 12:31:57 -0700

    Compile with latest Picotls version, use bcrypt on Windows

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -4,7 +4,7 @@
 # Build at a known-good commit
 # Must select a commit date (can copy-paste from git log)
 COMMIT_ID=a760bd5812441d3ef44fd34ae3c5aaab2016712d
-COMMIT_DATE="Sat Jul 7 12:26:35 2018 +0900"
+COMMIT_DATE="Tue Aug 7 09:01:25 2018 -0700"
 
 cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags --shallow-since="$COMMIT_DATE" https://github.com/h2o/picotls
