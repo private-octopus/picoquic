a0216a2ffe51508933fbe684a16f4da19387cce5
huitema <huitema@huitema.net>
2018-08-13 18:44:37 -0700

    Update checks to use 8/14/2018 version of Picotls

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -4,7 +4,7 @@
 # Build at a known-good commit
 # Must select a commit date (can copy-paste from git log)
 COMMIT_ID=a760bd5812441d3ef44fd34ae3c5aaab2016712d
-COMMIT_DATE="Tue Aug 7 09:01:25 2018 -0700"
+COMMIT_DATE="Tue Aug 14 10:13:45 2018 +0900"
 
 cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags --shallow-since="$COMMIT_DATE" https://github.com/h2o/picotls
