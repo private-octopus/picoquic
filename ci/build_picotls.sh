75f91b7a4aa5c94c3e066c927f6e9981e07d367b
huitema <huitema@huitema.net>
2018-10-03 10:27:30 -0700

    Align to "omit-eoed" commit of picotls

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -4,7 +4,7 @@
 # Build at a known-good commit
 # Must select a commit date (can copy-paste from git log)
 COMMIT_ID=a760bd5812441d3ef44fd34ae3c5aaab2016712d
-COMMIT_DATE="Wed Aug 22 11:16:13 2018 +0900"
+COMMIT_DATE="Wed Oct 3 11:06:54 2018 +0900"
 
 cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags --shallow-since="$COMMIT_DATE" https://github.com/h2o/picotls
