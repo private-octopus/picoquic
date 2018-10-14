9005b262057052fc1dcb7e40b5f0230f078b8b7e
Peter Wu <peter@lekensteyn.nl>
2018-10-14 15:52:22 +0200

    ci: stop using git clone --shallow-since
    When git merges are in use it appears that --shallow-since=DATE stops at
    the first merge commit that is older than DATE. This could create a too
    shallow clone that lacks the desired commit. Disable this optimization.
    Observed with git 2.19.1.
    
    Fixes #359

--- ci/build_picotls.ps1
+++ ci/build_picotls.ps1
@@ -1,7 +1,5 @@
 # Build at a known-good commit
-# Must select a commit date (can copy-paste from git log)
 $COMMIT_ID="8443c09c0f091482679e0b32c4f238928b7f5c1e"
-$COMMIT_DATE="Thu Oct 4 10:28:52 2018 +0900"
 
 # Match expectations of picotlsvs project.
 foreach ($dir in "$Env:OPENSSLDIR","$Env:OPENSSL64DIR") {
@@ -12,7 +10,7 @@ foreach ($dir in "$Env:OPENSSLDIR","$Env:OPENSSL64DIR") {
 }
 
 pushd ..
-git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags --shallow-since="$COMMIT_DATE" https://github.com/h2o/picotls 2>&1 | %{ "$_" }
+git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls 2>&1 | %{ "$_" }
 cd picotls
 git apply ..\picoquic\ci\picotls-win32.patch
 git checkout -q "$COMMIT_ID"
--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -2,12 +2,10 @@
 #build last picotls master (for Travis)
 
 # Build at a known-good commit
-# Must select a commit date (can copy-paste from git log)
 COMMIT_ID=8443c09c0f091482679e0b32c4f238928b7f5c1e
-COMMIT_DATE="Thu Oct 4 10:28:52 2018 +0900"
 
 cd ..
-git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags --shallow-since="$COMMIT_DATE" https://github.com/h2o/picotls
+git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 cd picotls
 git checkout "$COMMIT_ID"
 #git submodule init
