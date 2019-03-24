d1135b0ac5d57fa1fd67deb3f255af31f5f399d2
huitema <huitema@huitema.net>
2019-03-24 12:47:10 +0100

    Test picotls script update

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -8,8 +8,8 @@ cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 cd picotls
 git checkout "$COMMIT_ID"
-#git submodule init
-#git submodule update
+git submodule init
+git submodule update
 cmake $CMAKE_OPTS .
 make -j$(nproc) all
 cd ..
