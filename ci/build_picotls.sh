9c00826ba35faa16cf1124c82174f4aa5703e452
huitema <huitema@huitema.net>
2019-03-25 09:53:08 +0100

    Try different commit ID

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -2,14 +2,14 @@
 #build last picotls master (for Travis)
 
 # Build at a known-good commit
-COMMIT_ID=4e6080b6a1ede0d3b23c72a8be73b46ecaf1a084
+COMMIT_ID=03674790ae42c0d3675c5b462c52988f67454e11
 
 cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 cd picotls
 git checkout "$COMMIT_ID"
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 cmake $CMAKE_OPTS .
 make -j$(nproc) all
 cd ..
