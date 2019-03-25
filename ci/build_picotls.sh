7bbf8c4d4dc0c33cbf6220f63ac694fdd96dfe89
huitema <huitema@huitema.net>
2019-03-25 10:13:18 +0100

    Try not specifying a commit ID

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -7,7 +7,8 @@ COMMIT_ID=03674790ae42c0d3675c5b462c52988f67454e11
 cd ..
 git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
 cd picotls
-git checkout "$COMMIT_ID"
+# git checkout "$COMMIT_ID"
+git checkout
 # git submodule init
 # git submodule update
 cmake $CMAKE_OPTS .
