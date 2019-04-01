bbc4553c353b3f080521208e83f223429246895a
huitema <huitema@huitema.net>
2019-04-01 04:42:40 +0200

    Try straight download of picotls

--- ci/build_picotls.sh
+++ ci/build_picotls.sh
@@ -5,7 +5,8 @@
 COMMIT_ID=d18549f4586a43ec5fadbbdd1d0aa640a2bd75fe
 
 cd ..
-git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
+# git clone --branch master --single-branch --shallow-submodules --recurse-submodules --no-tags https://github.com/h2o/picotls
+git clone https://github.com/h2o/picotls
 cd picotls
 git checkout -q "$COMMIT_ID"
 git submodule init
