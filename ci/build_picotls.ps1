7e74cd638f3c88f8a462726000b2e1d0946d6bfd
huitema <huitema@huitema.net>
2019-07-09 15:31:53 -0700

    Trying yet another patch.

--- ci/build_picotls.ps1
+++ ci/build_picotls.ps1
@@ -2,11 +2,11 @@
 $COMMIT_ID="aa309cc0728dee0b4d45b0615fd04bf019e63e9c"
 
 # Match expectations of picotlsvs project.
+mkdir $dir\include\
 foreach ($dir in "$Env:OPENSSLDIR","$Env:OPENSSL64DIR") {
     if ($dir) {
         cp "$dir\lib\libcrypto.lib" "$dir"
-		mkdir $dir\include\ms
-        cp C:\OpenSSL-Win32\include\openssl\applink.c "$dir\ms"
+        cp C:\OpenSSL-Win32\include\openssl\applink.c "$dir\include\openssl"
     }
 }
 
@@ -14,9 +14,9 @@ pushd ..
 git clone https://github.com/h2o/picotls 2>&1 | %{ "$_" }
 cd picotls
 git checkout -q "$COMMIT_ID"
-git apply ..\picoquic\ci\picotls-win32.patch
 git submodule init
 git submodule update
+git apply ..\picoquic\ci\picotls-win32.patch
 
 msbuild "/p:Configuration=$Env:Configuration" "/p:Platform=$Env:Platform" /m picotlsvs\picotlsvs.sln
 
