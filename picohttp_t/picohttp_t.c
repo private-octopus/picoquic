5fe19a46d772b97232291d6c42cf01b8cabe5f11
steschu <steschu77@gmail.com>
2019-08-05 17:28:54 +0200

    Move http files into picohttp/picohttp_t/picohttptest

---
+++ picohttp_t/picohttp_t.c
@@ -0,0 +1,458 @@
+/*
+* Author: Christian Huitema
+* Copyright (c) 2017, Private Octopus, Inc.
+* All rights reserved.
+*
+* Permission to use, copy, modify, and distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Private Octopus, Inc. BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+#ifdef _WINDOWS
+#include "getopt.h"
+#endif
+#include "picoquic.h"
+#include "util.h"
+#include "picoquictest.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct st_picoquic_test_def_t {
+    char const* test_name;
+    int (*test_fn)();
+} picoquic_test_def_t;
+
+typedef enum {
+    test_not_run = 0,
+    test_excluded,
+    test_success,
+    test_failed
+} test_status_t;
+
+static const picoquic_test_def_t test_table[] = {
+    { "picohash", picohash_test },
+    { "splay", splay_test },
+    { "cnxcreation", cnxcreation_test },
+    { "parseheader", parseheadertest },
+    { "pn2pn64", pn2pn64test },
+    { "intformat", intformattest },
+    { "fnv1a", fnv1atest },
+    { "float16", float16test },
+    { "varint", varint_test },
+    { "sack", sacktest },
+    { "skip_frames", skip_frame_test },
+    { "parse_frames", parse_frame_test },
+    { "logger", logger_test },
+    { "TlsStreamFrame", TlsStreamFrameTest },
+    { "StreamZeroFrame", StreamZeroFrameTest },
+    { "stream_splay", stream_splay_test },
+    { "stream_output", stream_output_test },
+    { "split_stream_frame", split_stream_frame_test },
+    { "copy_for_retransmit", test_copy_for_retransmit },
+    { "sendack", sendacktest },
+    { "ackrange", ackrange_test },
+    { "ack_of_ack", ack_of_ack_test },
+    { "sim_link", sim_link_test },
+    { "clear_text_aead", cleartext_aead_test },
+    { "pn_ctr", pn_ctr_test },
+    { "cleartext_pn_enc", cleartext_pn_enc_test },
+    { "cid_global_encrypt", cid_global_encrypt_test },
+    { "cid_mask_encrypt", cid_mask_encrypt_test },
+    { "draft17_vector", draft17_vector_test },
+    { "pn_enc_1rtt", pn_enc_1rtt_test },
+    { "cnxid_stash", cnxid_stash_test },
+    { "new_cnxid", new_cnxid_test },
+    { "tls_api", tls_api_test },
+    { "null_sni", null_sni_test },
+    { "silence_test", tls_api_silence_test },
+    { "tls_api_version_negotiation", tls_api_version_negotiation_test },
+    { "first_loss", tls_api_client_first_loss_test },
+    { "second_loss", tls_api_client_second_loss_test },
+    { "SH_loss", tls_api_server_first_loss_test },
+    { "client_losses", tls_api_client_losses_test },
+    { "server_losses", tls_api_server_losses_test },
+    { "transport_param_stream_id", transport_param_stream_id_test },
+    { "stream_rank", stream_rank_test },
+    { "stream_id_to_rank", stream_id_to_rank_test},
+    { "transport_param", transport_param_test },
+    { "tls_api_sni", tls_api_sni_test },
+    { "tls_api_alpn", tls_api_alpn_test },
+    { "tls_api_wrong_alpn", tls_api_wrong_alpn_test },
+    { "tls_api_oneway_stream", tls_api_oneway_stream_test },
+    { "tls_api_q_and_r_stream", tls_api_q_and_r_stream_test },
+    { "tls_api_q2_and_r2_stream", tls_api_q2_and_r2_stream_test },
+    { "tls_api_server_reset", tls_api_server_reset_test },
+    { "tls_api_bad_server_reset", tls_api_bad_server_reset_test },
+    { "tls_api_very_long_stream", tls_api_very_long_stream_test },
+    { "tls_api_very_long_max", tls_api_very_long_max_test },
+    { "tls_api_very_long_with_err", tls_api_very_long_with_err_test },
+    { "tls_api_very_long_congestion", tls_api_very_long_congestion_test },
+    { "many_short_loss", many_short_loss_test },
+    { "http0dot9", http0dot9_test },
+    { "retry", tls_api_retry_test },
+    { "retry_token", tls_retry_token_test },
+    { "two_connections", tls_api_two_connections_test },
+    { "multiple_versions", tls_api_multiple_versions_test },
+    { "keep_alive", keep_alive_test },
+    { "sockets", socket_test },
+    { "socket_ecn", socket_ecn_test },
+    { "ticket_store", ticket_store_test },
+    { "token_store", token_store_test },
+    { "session_resume", session_resume_test },
+    { "zero_rtt", zero_rtt_test },
+    { "zero_rtt_loss", zero_rtt_loss_test },
+    { "stop_sending", stop_sending_test },
+    { "unidir", unidir_test },
+    { "mtu_discovery", mtu_discovery_test },
+    { "spurious_retransmit", spurious_retransmit_test },
+    { "tls_zero_share", tls_zero_share_test },
+    { "transport_param_log", transport_param_log_test },
+    { "bad_certificate", bad_certificate_test },
+    { "set_verify_certificate_callback_test", set_verify_certificate_callback_test },
+    { "virtual_time" , virtual_time_test },
+    { "different_params", tls_different_params_test },
+    { "quant_params", tls_quant_params_test },
+    { "set_certificate_and_key", set_certificate_and_key_test },
+    { "request_client_authentication", request_client_authentication_test },
+    { "bad_client_certificate", bad_client_certificate_test },
+    { "nat_rebinding", nat_rebinding_test },
+    { "nat_rebinding_loss", nat_rebinding_loss_test },
+    { "spin_bit", spin_bit_test},
+    { "client_error", client_error_test },
+    { "packet_enc_dec", packet_enc_dec_test},
+    { "pn_vector", cleartext_pn_vector_test },
+    { "zero_rtt_spurious", zero_rtt_spurious_test },
+    { "zero_rtt_retry", zero_rtt_retry_test },
+    { "random_tester", random_tester_test},
+    { "random_public_tester", random_public_tester_test},
+    { "transmit_cnxid", transmit_cnxid_test },
+    { "probe_api", probe_api_test },
+    { "migration" , migration_test },
+    { "migration_long", migration_test_long },
+    { "migration_with_loss", migration_test_loss },
+    { "preferred_address", preferred_address_test},
+    { "cnxid_renewal",  cnxid_renewal_test },
+    { "retire_cnxid", retire_cnxid_test },
+    { "not_before_cnxid", not_before_cnxid_test },
+    { "server_busy", server_busy_test },
+    { "initial_close", initial_close_test },
+    { "initial_server_close", initial_server_close_test },
+    { "new_rotated_key", new_rotated_key_test },
+    { "key_rotation", key_rotation_test },
+    { "false_migration", false_migration_test },
+    { "nat_handshake", nat_handshake_test },
+    { "key_rotation_vector", key_rotation_vector_test },
+    { "key_rotation_stress", key_rotation_stress_test },
+    { "short_initial_cid", short_initial_cid_test },
+    { "stream_id_max", stream_id_max_test },
+    { "padding_test", padding_test },
+    { "packet_trace", packet_trace_test },
+    { "rebiding_stress", rebinding_stress_test },
+    { "ready_to_send", ready_to_send_test },
+    { "cubic", cubic_test },
+    { "long_rtt", long_rtt_test },
+    { "cid_length", cid_length_test },
+    { "optimistic_ack", optimistic_ack_test },
+    { "optimistic_hole", optimistic_hole_test },
+    { "bad_coalesce", bad_coalesce_test },
+    { "document_addresses", document_addresses_test },
+    { "stress", stress_test },
+    { "fuzz", fuzz_test },
+    { "fuzz_initial", fuzz_initial_test},
+    { "h3zero_integer", h3zero_integer_test },
+    { "qpack_huffman", qpack_huffman_test },
+    { "h3zero_parse_qpack", h3zero_parse_qpack_test },
+    { "h3zero_prepare_qpack", h3zero_prepare_qpack_test },
+    { "h3zero_stream_test", h3zero_stream_test },
+    { "parse_demo_scenario", parse_demo_scenario_test },
+    { "h3zero_server", h3zero_server_test },
+    { "h09_server", h09_server_test },
+    { "generic_server", generic_server_test},
+    { "esni", esni_test }
+};
+
+static size_t const nb_tests = sizeof(test_table) / sizeof(picoquic_test_def_t);
+
+static int do_one_test(size_t i, FILE* F)
+{
+    int ret = 0;
+
+    if (i >= nb_tests) {
+        fprintf(F, "Invalid test number %" PRIst "\n", i);
+        ret = -1;
+    } else {
+        fprintf(F, "Starting test number %" PRIst ", %s\n", i, test_table[i].test_name);
+
+        fflush(F);
+
+        ret = test_table[i].test_fn();
+        if (ret == 0) {
+            fprintf(F, "    Success.\n");
+        } else {
+            fprintf(F, "    Fails, error: %d.\n", ret);
+        }
+    }
+
+    fflush(F);
+
+    return ret;
+}
+
+int usage(char const * argv0)
+{
+    fprintf(stderr, "PicoQUIC test execution\n");
+    fprintf(stderr, "Usage: picoquic_ct [-x <excluded>] [<list of tests]\n");
+    fprintf(stderr, "\nUsage: %s [test1 [test2 ..[testN]]]\n\n", argv0);
+    fprintf(stderr, "   Or: %s [-x test]*", argv0);
+    fprintf(stderr, "Valid test names are: \n");
+    for (size_t x = 0; x < nb_tests; x++) {
+        fprintf(stderr, "    ");
+
+        for (int j = 0; j < 4 && x < nb_tests; j++, x++) {
+            fprintf(stderr, "%s, ", test_table[x].test_name);
+        }
+        fprintf(stderr, "\n");
+    }
+    fprintf(stderr, "Options: \n");
+    fprintf(stderr, "  -x test           Do not run the specified test.\n");
+    fprintf(stderr, "  -s nnn            Run stress for nnn minutes.\n");
+    fprintf(stderr, "  -f nnn            Run fuzz for nnn minutes.\n");
+    fprintf(stderr, "  -n                Disable debug prints.\n");
+    fprintf(stderr, "  -r                Retry failed tests with debug print enabled.\n");
+    fprintf(stderr, "  -h                Print this help message\n");
+    fprintf(stderr, "  -S solution_dir   Set the path to the source files to find the default files\n");
+
+    return -1;
+}
+
+int get_test_number(char const * test_name)
+{
+    int test_number = -1;
+
+    for (size_t i = 0; i < nb_tests; i++) {
+        if (strcmp(test_name, test_table[i].test_name) == 0) {
+            test_number = (int)i;
+        }
+    }
+
+    return test_number;
+}
+
+int main(int argc, char** argv)
+{
+    int ret = 0;
+    int nb_test_tried = 0;
+    int nb_test_failed = 0;
+    int stress_minutes = 0;
+    int found_exclusion = 0;
+    test_status_t * test_status = (test_status_t *) calloc(nb_tests, sizeof(test_status_t));
+    int opt;
+    int do_fuzz = 0;
+    int do_stress = 0;
+    int disable_debug = 0;
+    int retry_failed_test = 0;
+
+    if (test_status == NULL)
+    {
+        fprintf(stderr, "Could not allocate memory.\n");
+        ret = -1;
+    }
+    else
+    {
+        while (ret == 0 && (opt = getopt(argc, argv, "f:s:S:x:nrh")) != -1) {
+            switch (opt) {
+            case 'x': {
+                int test_number = get_test_number(optarg);
+
+                if (test_number < 0) {
+                    fprintf(stderr, "Incorrect test name: %s\n", optarg);
+                    ret = usage(argv[0]);
+                }
+                else {
+                    test_status[test_number] = test_excluded;
+                    found_exclusion = 1;
+                }
+                break;
+            }
+            case 'f':
+                do_fuzz = 1;
+                stress_minutes = atoi(optarg);
+                if (stress_minutes <= 0) {
+                    fprintf(stderr, "Incorrect stress minutes: %s\n", optarg);
+                    ret = usage(argv[0]);
+                }
+                break;
+            case 's':
+                do_stress = 1;
+                stress_minutes = atoi(optarg);
+                if (stress_minutes <= 0) {
+                    fprintf(stderr, "Incorrect stress minutes: %s\n", optarg);
+                    ret = usage(argv[0]);
+                }
+                break;
+            case 'S':
+                picoquic_test_set_solution_dir(optarg);
+                break;
+            case 'n':
+                disable_debug = 1;
+                break;
+            case 'r':
+                retry_failed_test = 1;
+                break;
+            case 'h':
+                usage(argv[0]);
+exit(0);
+break;
+            default:
+                ret = usage(argv[0]);
+                break;
+            }
+        }
+
+        if (disable_debug) {
+            debug_printf_suspend();
+        }
+        else {
+            debug_printf_push_stream(stderr);
+        }
+
+        if (ret == 0 && stress_minutes > 0) {
+            if (optind >= argc && found_exclusion == 0) {
+                for (size_t i = 0; i < nb_tests; i++) {
+                    if (strcmp(test_table[i].test_name, "stress") == 0)
+                    {
+                        if (do_stress == 0) {
+                            test_status[i] = test_excluded;
+                        }
+                    }
+                    else if (strcmp(test_table[i].test_name, "fuzz") == 0) {
+                        if (do_fuzz == 0) {
+                            test_status[i] = test_excluded;
+                        }
+                    }
+                    else {
+                        test_status[i] = test_excluded;
+                    }
+                }
+                picoquic_stress_test_duration = stress_minutes;
+                picoquic_stress_test_duration *= 60000000;
+            }
+        }
+
+        if (ret == 0)
+        {
+            if (optind >= argc) {
+                for (size_t i = 0; i < nb_tests; i++) {
+                    if (test_status[i] == test_not_run) {
+                        nb_test_tried++;
+                        if (do_one_test(i, stdout) != 0) {
+                            test_status[i] = test_failed;
+                            nb_test_failed++;
+                            ret = -1;
+                        }
+                        else {
+                            test_status[i] = test_success;
+                        }
+                    }
+                    else if (stress_minutes == 0) {
+                        fprintf(stdout, "Test number %d (%s) is bypassed.\n", (int)i, test_table[i].test_name);
+                    }
+                }
+            }
+            else {
+                for (int arg_num = optind; arg_num < argc; arg_num++) {
+                    int test_number = get_test_number(argv[arg_num]);
+
+                    if (test_number < 0) {
+                        fprintf(stderr, "Incorrect test name: %s\n", argv[arg_num]);
+                        ret = usage(argv[0]);
+                    }
+                    else {
+                        nb_test_tried++;
+                        if (do_one_test(test_number, stdout) != 0) {
+                            test_status[test_number] = test_failed;
+                            nb_test_failed++;
+                            ret = -1;
+                        }
+                        else if (test_status[test_number] == test_not_run) {
+                            test_status[test_number] = test_success;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (nb_test_tried > 1) {
+            fprintf(stdout, "Tried %d tests, %d fail%s.\n", nb_test_tried,
+                nb_test_failed, (nb_test_failed > 1) ? "" : "s");
+        }
+
+        if (nb_test_failed > 0) {
+            fprintf(stdout, "Failed test(s): ");
+            for (size_t i = 0; i < nb_tests; i++) {
+                if (test_status[i] == test_failed) {
+                    fprintf(stdout, "%s ", test_table[i].test_name);
+                }
+            }
+            fprintf(stdout, "\n");
+
+            if (disable_debug && retry_failed_test) {
+                /* debug_printf_push_stream(stderr); */
+                debug_printf_resume();
+                ret = 0;
+                for (size_t i = 0; i < nb_tests; i++) {
+                    int is_fuzz = 0;
+                    if (strcmp("stress", test_table[i].test_name) == 0 ||
+                        strcmp("fuzz", test_table[i].test_name) == 0 ||
+                        strcmp("fuzz_initial", test_table[i].test_name) == 0) {
+                        is_fuzz = 1;
+                    }
+                    if (test_status[i] == test_failed) {
+                        fprintf(stdout, "Retrying %s:\n", test_table[i].test_name);
+                        if (is_fuzz) {
+                            debug_printf_suspend();
+                        }
+                        if (do_one_test(i, stdout) != 0) {
+                            test_status[i] = test_failed;
+                            ret = -1;
+                        }
+                        else {
+                            /* This was a Heisenbug.. */
+                            test_status[i] = test_success;
+                        }
+                        if (is_fuzz) {
+                            debug_printf_resume();
+                        }
+                    }
+                }
+                if (ret == 0) {
+                    fprintf(stdout, "All tests pass after second try.\n");
+                }
+                else {
+                    fprintf(stdout, "Still failing: ");
+                    for (size_t i = 0; i < nb_tests; i++) {
+                        if (test_status[i] == test_failed) {
+                            fprintf(stdout, "%s ", test_table[i].test_name);
+                        }
+                    }
+                    fprintf(stdout, "\n");
+                }
+            }
+        }
+
+        free(test_status);
+    }
+    return (ret);
+}
---
+++ picohttptest/picohttptest_internal.h
@@ -0,0 +1,220 @@
+/*
+* Author: Christian Huitema
+* Copyright (c) 2017, Private Octopus, Inc.
+* All rights reserved.
+*
+* Permission to use, copy, modify, and distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Private Octopus, Inc. BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef PICOQUICTEST_INTERNAL_H
+#define PICOQUICTEST_INTERNAL_H
+
+#include "picoquic_internal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* SNI, ALPN, Stores and Certificates used for test
+ */
+
+#define PICOQUIC_TEST_SNI "test.example.com"
+#define PICOQUIC_TEST_ALPN "picoquic-test"
+
+#ifdef _WINDOWS
+#define PICOQUIC_TEST_FILE_SERVER_CERT "certs\\cert.pem"
+#define PICOQUIC_TEST_FILE_SERVER_BAD_CERT "certs\\badcert.pem"
+#define PICOQUIC_TEST_FILE_SERVER_KEY "certs\\key.pem"
+#define PICOQUIC_TEST_FILE_CERT_STORE "certs\\test-ca.crt"
+#define PICOQUIC_TEST_FILE_ESNI_KEY "certs\\esni-secp256r1.key"
+#define PICOQUIC_TEST_FILE_ESNI_RR "certs\\esni-rr.bin"
+#else
+#define PICOQUIC_TEST_FILE_SERVER_CERT "certs/cert.pem"
+#define PICOQUIC_TEST_FILE_SERVER_BAD_CERT "certs/badcert.pem"
+#define PICOQUIC_TEST_FILE_SERVER_KEY "certs/key.pem"
+#define PICOQUIC_TEST_FILE_CERT_STORE "certs/test-ca.crt"
+#define PICOQUIC_TEST_FILE_ESNI_KEY "certs/esni-secp256r1.key"
+#define PICOQUIC_TEST_FILE_ESNI_RR "certs/esni-rr.bin"
+#endif
+
+ /* To set the solution directory for tests */
+extern char const * picoquic_test_solution_dir;
+
+/* Really basic network simulator, only simulates a simple link using a
+ * packet structure.
+ * Init: link creation. Returns a link structure with defined bandwidth,
+ * latency, loss pattern and initial time. The link is empty. The loss
+ * pattern is a 64 bit bit mask.
+ * Submit packet of length L at time t. The packet is queued to the link.
+ * Get packet out of link at time T + L + Queue.
+ */
+
+typedef struct st_picoquictest_sim_packet_t {
+    struct st_picoquictest_sim_packet_t* next_packet;
+    uint64_t arrival_time;
+    size_t length;
+    struct sockaddr_storage addr_from;
+    struct sockaddr_storage addr_to;
+    uint8_t bytes[PICOQUIC_MAX_PACKET_SIZE];
+} picoquictest_sim_packet_t;
+
+typedef struct st_picoquictest_sim_link_t {
+    uint64_t next_send_time;
+    uint64_t queue_time;
+    uint64_t queue_delay_max;
+    uint64_t picosec_per_byte;
+    uint64_t microsec_latency;
+    uint64_t* loss_mask;
+    uint64_t packets_dropped;
+    uint64_t packets_sent;
+    picoquictest_sim_packet_t* first_packet;
+    picoquictest_sim_packet_t* last_packet;
+} picoquictest_sim_link_t;
+
+#define PICOQUIC_TEST_SNI "test.example.com"
+#define PICOQUIC_TEST_ALPN "picoquic-test"
+#define PICOQUIC_TEST_WRONG_ALPN "picoquic-bla-bla"
+#define PICOQUIC_TEST_MAX_TEST_STREAMS 18
+
+typedef enum {
+    test_api_fail_data_on_unknown_stream = 1,
+    test_api_fail_recv_larger_than_sent = 2,
+    test_api_fail_fin_received_twice = 4,
+    test_api_fail_cannot_send_response = 8,
+    test_api_fail_cannot_send_query = 16,
+    test_api_fail_data_does_not_match = 32,
+    test_api_fail_unexpected_frame = 64,
+    test_api_bad_stream0_data = 128
+} test_api_fail_mode;
+
+typedef struct st_test_api_stream_desc_t {
+    uint64_t stream_id;
+    uint64_t previous_stream_id;
+    size_t q_len;
+    size_t r_len;
+} test_api_stream_desc_t;
+
+typedef struct st_test_api_stream_t {
+    uint64_t stream_id;
+    uint64_t previous_stream_id;
+    int q_sent;
+    int r_sent;
+    picoquic_call_back_event_t q_received;
+    picoquic_call_back_event_t r_received;
+    size_t q_len;
+    size_t q_recv_nb;
+    size_t r_len;
+    size_t r_recv_nb;
+    uint8_t* q_src;
+    uint8_t* q_rcv;
+    uint8_t* r_src;
+    uint8_t* r_rcv;
+} test_api_stream_t;
+
+typedef struct st_test_api_callback_t {
+    int client_mode;
+    int fin_received;
+    int error_detected;
+    uint32_t nb_bytes_received;
+} test_api_callback_t;
+
+typedef struct st_picoquic_test_tls_api_ctx_t {
+    picoquic_quic_t* qclient;
+    picoquic_quic_t* qserver;
+    picoquic_cnx_t* cnx_client;
+    picoquic_cnx_t* cnx_server;
+    int client_use_nat;
+    int server_use_multiple_addresses;
+    int do_bad_coalesce_test;
+    struct sockaddr_in client_addr;
+    struct sockaddr_in server_addr;
+    test_api_callback_t client_callback;
+    test_api_callback_t server_callback;
+    size_t nb_test_streams;
+    test_api_stream_t test_stream[PICOQUIC_TEST_MAX_TEST_STREAMS];
+    picoquictest_sim_link_t* c_to_s_link;
+    picoquictest_sim_link_t* s_to_c_link;
+    int received_version_negotiation;
+
+    /* Stream 0 is reserved for the "infinite stream" simulation */
+    size_t stream0_target;
+    size_t stream0_sent;
+    size_t stream0_received;
+    int stream0_test_option;
+
+    int sum_data_received_at_server;
+    int sum_data_received_at_client;
+    int test_finished;
+    int streams_finished;
+    int reset_received;
+} picoquic_test_tls_api_ctx_t;
+
+typedef struct st_test_skip_frames_t {
+    char const* name;
+    uint8_t* val;
+    size_t len;
+    int is_pure_ack;
+    int must_be_last;
+    int epoch;
+} test_skip_frames_t;
+
+extern test_skip_frames_t test_skip_list[];
+
+extern size_t nb_test_skip_list;
+
+picoquictest_sim_link_t* picoquictest_sim_link_create(double data_rate_in_gps,
+    uint64_t microsec_latency, uint64_t* loss_mask, uint64_t queue_delay_max, uint64_t current_time);
+
+void picoquictest_sim_link_delete(picoquictest_sim_link_t* link);
+
+picoquictest_sim_packet_t* picoquictest_sim_link_create_packet();
+
+uint64_t picoquictest_sim_link_next_arrival(picoquictest_sim_link_t* link, uint64_t current_time);
+
+picoquictest_sim_packet_t* picoquictest_sim_link_dequeue(picoquictest_sim_link_t* link,
+    uint64_t current_time);
+
+void picoquictest_sim_link_submit(picoquictest_sim_link_t* link, picoquictest_sim_packet_t* packet,
+    uint64_t current_time);
+
+int tls_api_init_ctx(picoquic_test_tls_api_ctx_t** pctx, uint32_t proposed_version,
+    char const* sni, char const* alpn, uint64_t* p_simulated_time,
+    char const* ticket_file_name, char const* token_file_name, 
+    int force_zero_share, int delayed_init, int use_bad_crypt);
+
+void tls_api_delete_ctx(picoquic_test_tls_api_ctx_t* test_ctx);
+
+int tls_api_connection_loop(picoquic_test_tls_api_ctx_t* test_ctx,
+    uint64_t* loss_mask, uint64_t queue_delay_max, uint64_t* simulated_time);
+
+int tls_api_one_sim_round(picoquic_test_tls_api_ctx_t* test_ctx,
+    uint64_t* simulated_time, uint64_t time_out, int* was_active);
+
+void picoquic_set_test_address(struct sockaddr_in * addr, uint32_t addr_val, uint16_t port);
+
+int test_one_pn_enc_pair(uint8_t * seqnum, size_t seqnum_len, void * pn_enc, void * pn_dec, uint8_t * sample);
+
+int picoquic_test_compare_files(char const* fname1, char const* fname2);
+
+uint64_t picoquic_test_random(uint64_t * random_context);
+void picoquic_test_random_bytes(uint64_t * random_context, uint8_t * bytes, size_t bytes_max);
+uint64_t picoquic_test_uniform_random(uint64_t * random_context, uint64_t rnd_max);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
