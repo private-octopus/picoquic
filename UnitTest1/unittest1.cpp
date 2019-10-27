3c698008b152cf12ec0f55ed5dfbe70e23ebb39c
Christian Huitema <huitema@huitema.net>
2019-10-26 20:35:44 -0700

    Add fast NAT rebinding test to try elicit migartion bug.

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -692,6 +692,13 @@ namespace UnitTest1
             Assert::AreEqual(ret, 0);
         }
 
+        TEST_METHOD(fast_nat_rebinding)
+        {
+            int ret = fast_nat_rebinding_test();
+
+            Assert::AreEqual(ret, 0);
+        }
+
         TEST_METHOD(test_spin_bit)
         {
             int ret = spin_bit_test();
