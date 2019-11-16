15b3465aaa2b10ca658abd57473156b18d0727f9
Christian Huitema <huitema@huitema.net>
2019-11-16 17:10:22 +0800

    More compile fixes for quicproto

--- picoqinq/qinqproto.c
+++ picoqinq/qinqproto.c
@@ -18,6 +18,9 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
+
+#include <stdlib.h>
+#include <string.h>
 #include "util.h"
 #include "qinqproto.h"
 
