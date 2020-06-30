42c6200fb20217e31c07b2c3ac73dadbd0c337b8
Christian Huitema <huitema@huitema.net>
2020-06-30 12:17:24 -0700

    Add get transport parameters API

--- picoquic/picoquic.h
+++ picoquic/picoquic.h
@@ -434,6 +434,8 @@ int picoquic_set_key_exchange(picoquic_quic_t* quic, int key_exchange_id);
 int picoquic_set_default_tp(picoquic_quic_t* quic, picoquic_tp_t* tp);
 /* Set the transport parameters per connection */
 void picoquic_set_transport_parameters(picoquic_cnx_t * cnx, picoquic_tp_t const * tp);
+/* Get the transport parameters per connection */
+picoquic_tp_t const* picoquic_get_transport_parameters(picoquic_cnx_t* cnx, int get_local);
 
 /* Set the TLS certificate chain(DER format) for the QUIC context. The context will take ownership over the certs pointer. */
 void picoquic_set_tls_certificate_chain(picoquic_quic_t* quic, ptls_iovec_t* certs, size_t count);
