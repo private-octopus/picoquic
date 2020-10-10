a18969179823b1e08d852b2b3db8f3996dcdb94a
Christian Huitema <huitema@huitema.net>
2020-10-10 16:55:10 -0700

    Fix handling of work directories

--- ci/build_picoquic.ps1
+++ ci/build_picoquic.ps1
@@ -1,6 +1,7 @@
-if ($Env:Platform -eq "x64") { cd x64 }
-cd "$Env:Configuration"
+if ($Env:Platform -eq "x64") { mkdir -Force x64; cd x64 }
+mkdir -Force $Env:Configuration"
 pwd
-
-msbuild "/p:Configuration=$Env:Configuration" "/p:Platform=$Env:Platform" /m picotlsvs\picoquic.sln
+if ($Env:Platform -eq "x64") { cd .. }
+pwd
+msbuild "/p:Configuration=$Env:Configuration" "/p:Platform=$Env:Platform" /m picoquic.sln
 
