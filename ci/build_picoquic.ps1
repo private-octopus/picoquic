af98b6d3c7c369fe27ba5032a33b76973d182213
Christian Huitema <huitema@huitema.net>
2020-10-10 17:24:53 -0700

    More powershell fixes

--- ci/build_picoquic.ps1
+++ ci/build_picoquic.ps1
@@ -1,7 +1,5 @@
-if ($Env:Platform -eq "x64") { mkdir -Force x64; cd x64 }
-mkdir -Force $Env:Configuration"
+if ($Env:Platform -eq "x64") { mkdir -Force x64; mkdir -Force x64\$Env:Configuration }
+else { mkdir -Force $Env:Configuration }
 pwd
-if ($Env:Platform -eq "x64") { cd .. }
-pwd
-msbuild /p:Configuration=$Env:Configuration /p:Platform=$Env:Platform /m .\picoquic.sln
+msbuild "/p:Configuration=$Env:Configuration" "/p:Platform=$Env:Platform" /m .\picoquic.sln
 
