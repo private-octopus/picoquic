8372e4a8b9fd6e652068dc427a1dec22e02b8e2a
Christian Huitema <huitema@huitema.net>
2020-10-10 18:23:46 -0700

    Restore yaml to previous value

--- ci/build_picoquic.ps1
+++
@@ -1,5 +0,0 @@
-if ($Env:Platform -eq "x64") { mkdir -Force x64; mkdir -Force x64\$Env:Configuration }
-else { mkdir -Force $Env:Configuration }
-pwd
-msbuild "/p:Configuration=$Env:Configuration" "/p:Platform=$Env:Platform" /m .\picoquic.sln
-
