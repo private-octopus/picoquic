5f55573736c2ae1b3eed66bde403cda031668e6e
Christian Huitema <huitema@huitema.net>
2021-07-30 09:38:59 -0700

    remove openssl patch attempt from picotls build

--- ci/build_picotls.ps1
+++ ci/build_picotls.ps1
@@ -1,15 +1,6 @@
 # Build at a known-good commit
 $COMMIT_ID="9accdf4af580e2ad883c929f8ca7a4cc58f15379"
 
-# Match expectations of picotlsvs project.
-mkdir $dir\include\
-foreach ($dir in "$Env:OPENSSLDIR","$Env:OPENSSL64DIR") {
-    if ($dir) {
-        cp "$dir\lib\libcrypto.lib" "$dir"
-        cp C:\OpenSSL-Win32\include\openssl\applink.c "$dir\include\openssl"
-    }
-}
-
 pushd ..
 git clone https://github.com/h2o/picotls 2>&1 | %{ "$_" }
 cd picotls
