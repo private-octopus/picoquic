fc4d269fce7f71fc1f8f99c431c6e65b66609e9b
Christian Huitema <huitema@huitema.net>
2022-01-09 10:41:29 -1000

    Restore full list of bad frames.

--- picoquictest/skip_frame_test.c
+++ picoquictest/skip_frame_test.c
@@ -485,7 +485,6 @@ static uint8_t test_frame_type_bad_frame_id[] = {
 };
 
 test_skip_frames_t test_frame_error_list[] = {
-#if 0
     TEST_SKIP_ITEM("bad_reset_stream_offset", test_frame_type_bad_reset_stream_offset, 0, 0, 3),
     TEST_SKIP_ITEM("bad_reset_stream", test_frame_type_bad_reset_stream, 0, 0, 3),
     TEST_SKIP_ITEM("bad_connection_close", test_type_bad_connection_close, 0, 0, 3),
@@ -506,14 +505,10 @@ test_skip_frames_t test_frame_error_list[] = {
     TEST_SKIP_ITEM("bad_abandon_0", test_frame_type_path_abandon_bad_0, 0, 1, 3),
     TEST_SKIP_ITEM("bad_abandon_1", test_frame_type_path_abandon_bad_1, 0, 0, 3),
     TEST_SKIP_ITEM("bad_abandon_2", test_frame_type_path_abandon_bad_2, 0, 0, 3),
-#endif
-    TEST_SKIP_ITEM("bad_bdp", test_frame_type_bdp_bad, 1, 0, 3)
-#if 0
-    ,
+    TEST_SKIP_ITEM("bad_bdp", test_frame_type_bdp_bad, 1, 0, 3),
     TEST_SKIP_ITEM("bad_bdp", test_frame_type_bdp_bad_addr, 1, 0, 3),
     TEST_SKIP_ITEM("bad_bdp", test_frame_type_bdp_bad_length, 1, 0, 3),
     TEST_SKIP_ITEM("bad_frame_id", test_frame_type_bad_frame_id, 1, 0, 3)
-#endif
 };
 
 size_t nb_test_frame_error_list = sizeof(test_frame_error_list) / sizeof(test_skip_frames_t);
