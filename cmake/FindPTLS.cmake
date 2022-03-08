1c25757197e84daec6c63351eb79315d2c14dddc
Christian Huitema <huitema@huitema.net>
2022-03-08 11:23:15 -0800

    Make ptls fusion library optional

--- cmake/FindPTLS.cmake
+++ cmake/FindPTLS.cmake
@@ -12,19 +12,33 @@ set(PTLS_HINTS ${PTLS_PREFIX}/lib ${CMAKE_BINARY_DIR}/../picotls ../picotls)
 find_library(PTLS_CORE_LIBRARY picotls-core HINTS ${PTLS_HINTS})
 find_library(PTLS_OPENSSL_LIBRARY picotls-openssl HINTS ${PTLS_HINTS})
 find_library(PTLS_FUSION_LIBRARY picotls-fusion HINTS ${PTLS_HINTS})
+if(NOT PTLS_FUSION_LIBRARY)
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
+    # if all listed variables are TRUE
+    find_package_handle_standard_args(PTLS REQUIRED_VARS
+        PTLS_CORE_LIBRARY
+        PTLS_OPENSSL_LIBRARY
+        PTLS_INCLUDE_DIR)
+    if(PTLS_FOUND)
+        set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY} )
+        set(PTLS_INCLUDE_DIRS ${PTLS_INCLUDE_DIR})
+        set(PTLS_WITH_FUSION_DEFAULT OFF)
+    endif()
+else()
+    include(FindPackageHandleStandardArgs)
+    # handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
+    # if all listed variables are TRUE
+    find_package_handle_standard_args(PTLS REQUIRED_VARS
+        PTLS_CORE_LIBRARY
+        PTLS_OPENSSL_LIBRARY
+        PTLS_INCLUDE_DIR)
 
-include(FindPackageHandleStandardArgs)
-# handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
-# if all listed variables are TRUE
-find_package_handle_standard_args(PTLS REQUIRED_VARS
-    PTLS_CORE_LIBRARY
-    PTLS_OPENSSL_LIBRARY
-    PTLS_FUSION_LIBRARY
-    PTLS_INCLUDE_DIR)
-
-if(PTLS_FOUND)
-    set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY} ${PTLS_FUSION_LIBRARY})
-    set(PTLS_INCLUDE_DIRS ${PTLS_INCLUDE_DIR})
+    if(PTLS_FOUND)
+        set(PTLS_LIBRARIES ${PTLS_CORE_LIBRARY} ${PTLS_OPENSSL_LIBRARY})
+        set(PTLS_INCLUDE_DIRS ${PTLS_INCLUDE_DIR})
+        set(PTLS_WITH_FUSION_DEFAULT ON)
+    endif()
 endif()
 
 mark_as_advanced(PTLS_LIBRARIES PTLS_INCLUDE_DIRS)
