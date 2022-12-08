f107f7d40ebdf2f2bbe8f281bfed06d4d3b2dfeb
Christian Huitema <huitema@huitema.net>
2022-12-08 13:36:44 -1000

    Do not mess with random alloc

--- picoquictest/tls_api_test.c
+++ picoquictest/tls_api_test.c
@@ -4007,11 +4007,7 @@ int zero_rtt_many_losses_test()
     for (int i = 0; ret == 0 && i < 50; i++)
     {
         uint64_t loss_mask = 0;
-#if 1
-        if (i < 25) {
-            continue;
-        }
-#endif
+
         for (int j = 0; j < 64; j++)
         {
             loss_mask <<= 1;
@@ -4020,6 +4016,11 @@ int zero_rtt_many_losses_test()
                 loss_mask |= 1;
             }
         }
+#if 1
+        if (i < 25) {
+            continue;
+        }
+#endif
 
         ret = zero_rtt_test_one(0, 0, loss_mask, 0, 0, 0, 0);
         if (ret != 0) {
