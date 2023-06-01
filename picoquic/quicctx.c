0ea36562a1c70fbd739dca79ad962fa4e235bc12
Christian Huitema <huitema@huitema.net>
2023-06-01 09:16:48 -0700

    Default to no reordering threshold.

--- picoquic/quicctx.c
+++ picoquic/quicctx.c
@@ -3559,7 +3559,7 @@ picoquic_cnx_t* picoquic_create_cnx(picoquic_quic_t* quic,
          * but this behavior creates too many ACKS on high speed links, so picoquic will request
          * the peer to not do that if the "delayed ACK" extension is available (ack_ignore_order_local = 1)
          */
-        cnx->ack_reordering_threshold_local = 1;
+        cnx->ack_reordering_threshold_local = 0;
         cnx->ack_reordering_threshold_remote = 0;
 
         cnx->latest_progress_time = start_time;
