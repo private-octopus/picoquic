410907fbbd15f6abcafbe51624c6988ee55bdfcb
Christian Huitema <huitema@huitema.net>
2023-11-03 20:41:58 -0700

    find package handle standard args

--- cmake/FindMbedTLS.cmake
+++ cmake/FindMbedTLS.cmake
@@ -13,13 +13,28 @@ find_path(MBEDTLS_INCLUDE_DIRS
         ${CMAKE_BINARY_DIR}/../mbedtls/include/
         ../mbedtls/include/ )
 
+message(STATUS "MBEDTLS_INCLUDE_DIRS: ${MBEDTLS_INCLUDE_DIRS}")
+
 set(MBEDTLS_HINTS ${MBEDTLS_PREFIX}/build/library 
     ${CMAKE_BINARY_DIR}/../mbedtls/build/library
     ../mbedtls/build/library ../mbedtls/library)
 
 find_library(MBEDTLS_LIBRARY mbedtls HINTS ${MBEDTLS_HINTS})
 find_library(MBEDTLS_CRYPTO mbedcrypto HINTS ${MBEDTLS_HINTS})
+message(STATUS "MBEDTLS_CRYPTO: ${MBEDTLS_CRYPTO}")
 find_library(MBEDTLS_X509 mbedx509 HINTS ${MBEDTLS_HINTS})
-set(MBEDTLS_LIBRARIES ${MBEDTLS_LIBRARY} ${MBEDTLS_CRYPTO} ${MBEDTLS_X509})
+message(STATUS "MBEDTLS_X509: ${MBEDTLS_X509}")
 
-mark_as_advanced(MBEDTLS_LIBRARY MBEDTLS_INCLUDE_DIRS)
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set PTLS_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(MbedTLS REQUIRED_VARS
+    MBEDTLS_LIBRARY
+    MBEDTLS_CRYPTO
+    MBEDTLS_X509
+    MBEDTLS_INCLUDE_DIRS)
+    
+if (MbedTLS_FOUND)
+    set(MBEDTLS_LIBRARIES ${MBEDTLS_LIBRARY} ${MBEDTLS_CRYPTO} ${MBEDTLS_X509})
+    mark_as_advanced(MBEDTLS_LIBRARIES MBEDTLS_INCLUDE_DIRS)
+endif ()
