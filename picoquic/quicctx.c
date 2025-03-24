7eed3c7f67ab53f2a36735982db38f664e55c530
Alexander Rabitsch <alexander.rabitsch@kau.se>
2025-03-24 11:04:17 +0100

    prevent crash if path callbacks are not disabled

--- picoquic/quicctx.c
+++ picoquic/quicctx.c
@@ -1703,7 +1703,7 @@ void picoquic_delete_path(picoquic_cnx_t* cnx, int path_index)
     }
 
     /* Signal to the application */
-    if (cnx->are_path_callbacks_enabled &&
+    if (cnx->are_path_callbacks_enabled && cnx->callback_fn != NULL &&
         cnx->callback_fn(cnx, path_x->unique_path_id, NULL, 0, picoquic_callback_path_deleted,
         cnx->callback_ctx, path_x->app_path_ctx) != 0) {
         picoquic_connection_error_ex(cnx, PICOQUIC_TRANSPORT_INTERNAL_ERROR, 0, "Path deleted callback failed.");
