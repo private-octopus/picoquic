e0771f089a589a838074d326efd8b068da64977c
huitema <huitema@huitema.net>
2025-11-17 23:40:22 -0800

    Loosen the prague vs prague test

--- picoquictest/cc_compete_test.c
+++ picoquictest/cc_compete_test.c
@@ -112,7 +112,7 @@ int cc_compete_prague2_test()
     spec.background_start_time = 0;
     spec.background_scenario_text = cc_compete_batch_scenario_10M;
     spec.nb_connections = 2;
-    spec.main_target_time = 2900000;
+    spec.main_target_time = 5000000;
     spec.data_rate_in_gbps = 0.05;
     spec.latency = 25000;
     spec.l4s_max = 15000;
