9da294a0edbbf2e95a0449337d6917e599eccaf9
huitema <huitema@huitema.net>
2026-02-24 11:11:05 -0800

    fix missing }

--- picoquic/quicctx.c
+++ picoquic/quicctx.c
@@ -5480,7 +5480,8 @@ void picoquic_debug_multithread_check(picoquic_quic_t* quic)
     if (quic->thread_id != 0) {
         if (quic->thread_id != picoquic_current_thread_id()) {
             DBG_PRINTF("Thread check failed: current thread %p, expected thread %p\n",
-            (void*)picoquic_current_thread_id(), (void*)quic->thread_id);
+                (void*)picoquic_current_thread_id(), (void*)quic->thread_id);
+        }
         abort();
     }
 }
