d16f34786aeab2c61a439a9d110ebbe7a1ad727e
huitema <huitema@huitema.net>
2026-04-20 10:59:05 -0700

    Do not compile unused qmux socket function

--- picoquic/quicctx.c
+++ picoquic/quicctx.c
@@ -2766,13 +2766,14 @@ void picoquic_default_quality_update(picoquic_quic_t* quic, uint64_t pacing_rate
     quic->rtt_update_delta = rtt_delta;
 }
 
-
+#if 0
 /* Setting the socket creation function */
 
 int picoquic_set_socket_fn(picoquic_quic_t* quic, picoquic_create_socket_fn socket_fn, void* create_socket_ctx)
 {
     return 0;
 }
+#endif
 
 /* management of path */
 
