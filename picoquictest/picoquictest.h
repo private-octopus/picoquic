518c46dce9cb85535fa9f168d118dc95a242d008
huitema <huitema@huitema.net>
2026-05-18 22:45:32 -0700

    qmux: cover stream ordering edges

--- picoquictest/picoquictest.h
+++ picoquictest/picoquictest.h
@@ -262,6 +262,7 @@ int qmux_receive_prohibited_frames_test(void);
 int qmux_receive_record_errors_test(void);
 int qmux_receive_extension_tp_ignore_test(void);
 int qmux_receive_stream_order_test(void);
+int qmux_receive_stream_order_edges_test(void);
 int qmux_receive_empty_record_test(void);
 int qmux_receive_split_record_test(void);
 int qmux_receive_qx_ping_test(void);
