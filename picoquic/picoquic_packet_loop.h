b9dc4a8fe005a13d0a28dc2df0f41debb066fe18
Christian Huitema <huitema@huitema.net>
2026-09-24 18:50:17 -0700

    Update picoquic/picoquic_packet_loop.h

--- picoquic/picoquic_packet_loop.h
+++ picoquic/picoquic_packet_loop.h
@@ -475,7 +475,7 @@ struct st_picoquic_network_thread_ctx_t* picoquic_get_thread_ctx(picoquic_quic_t
 
 /* Set a server context, using more parameters than the simple
 * creation from configuration.
-* config->qlog_dir is set; call picoquic_set_qlog() on *qserver
+*
 * afterwards if needed (see loglib/autoqlog.h).
 */
 int picoquic_server_set_context(picoquic_quic_t** qserver,
